b67b22ed7bbc5a7c1a6471b0d27a7e7a9d88a086
tammi <tammichng.co@gmail.com>
2020-09-04 17:20:18 +0800

    add dashboard

---
+++ frontend/dashboard/src/Demo/StockDashboard/Default.js
@@ -0,0 +1,269 @@
+import React from 'react';
+import {Row, Col, Card, Table, Tabs, Tab} from 'react-bootstrap';
+
+import Aux from "../../hoc/_Aux";
+import DEMO from "../../store/constant";
+import NVD3Chart from 'react-nvd3';
+
+import avatar1 from '../../assets/images/user/avatar-1.jpg';
+import avatar2 from '../../assets/images/user/avatar-2.jpg';
+import avatar3 from '../../assets/images/user/avatar-3.jpg';
+
+function getDatum() {
+    var sin = [],
+        sin2 = [],
+        cos = [];
+    for (var i = 0; i < 100; i++) {
+        sin.push({
+            'x': i,
+            'y': Math.sin(i / 10)
+        });
+        sin2.push({
+            'x': i,
+            'y': Math.sin(i / 10) * 0.25 + 0.5
+        });
+        cos.push({
+            'x': i,
+            'y': .5 * Math.cos(i / 10)
+        });
+    }
+    return [
+        {
+            values: sin,
+            key: 'Sine Wave',
+            color: '#A389D4'
+        },
+        {
+            values: cos,
+            key: 'Cosine Wave',
+            color: '#04a9f5'
+        },
+        {
+            values: sin2,
+            key: 'Another sine wave',
+            color: '#1de9b6',
+            area: true
+        }
+    ];
+}
+
+class StockDashboard extends React.Component {
+    render() {
+        const data = getDatum();
+
+        const tabContent = (
+            <Aux>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Silje Larsen</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-up f-22 m-r-10 text-c-green"/>3784</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Julie Vad</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-up f-22 m-r-10 text-c-green"/>3544</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar3} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Storm Hanse</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-down f-22 m-r-10 text-c-red"/>2739</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Frida Thomse</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-down f-22 m-r-10 text-c-red"/>1032</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Silje Larsen</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-up f-22 m-r-10 text-c-green"/>8750</span>
+                    </div>
+                </div>
+         
+            </Aux>
+        );
+
+        return (
+            <Aux>
+                <Row>
+
+                    <Col xl={4}>
+                        <Card className='card-social'>
+                            <Card.Body className='border-bottom'>
+                                <div className="row align-items-center justify-content-center">
+                                    <div className="col-auto">
+                                        <i className="fa fa-google-plus text-c-red f-36"/>
+                                    </div>
+                                    <div className="col text-right">
+                                        <h3>10,500</h3>
+                                        <h5 className="text-c-blue mb-0">+5.9% <span className="text-muted">Total Likes</span></h5>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                            <Card.Body>
+                                <div className="row align-items-center justify-content-center card-active">
+                                    <div className="col-6">
+                                        <h6 className="text-center m-b-10"><span className="text-muted m-r-5">Target:</span>25,998</h6>
+                                        <div className="progress">
+                                            <div className="progress-bar progress-c-theme" role="progressbar" style={{width: '80%', height: '6px'}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"/>
+                                        </div>
+                                    </div>
+                                    <div className="col-6">
+                                        <h6 className="text-center  m-b-10"><span className="text-muted m-r-5">Duration:</span>900</h6>
+                                        <div className="progress">
+                                            <div className="progress-bar progress-c-theme2" role="progressbar" style={{width: '50%', height: '6px'}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    
+                    <Col xl={4}>
+                        <Card>
+                            <Card.Body>
+                                <h6 className='mb-4'>Yearly Sales</h6>
+                                <div>
+                                    {
+                                        React.createElement(NVD3Chart, {
+                                            xAxis: {
+                                                tickFormat: function(d){ return d; },
+                                                axisLabel: 'Time (ms)'
+                                            },
+                                            yAxis: {
+                                                axisLabel: 'Voltage (v)',
+                                                tickFormat: function(d) {return parseFloat(d).toFixed(2); }
+                                            },
+                                            type:'lineChart',
+                                            datum: data,
+                                            x: 'x',
+                                            y: 'y',
+                                            height: 300,
+                                            renderEnd: function(){
+                                                console.log('renderEnd');
+                                            }
+                                        })
+                                    }
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={8} xl={10}>
+                        <Tabs defaultActiveKey="today" id="uncontrolled-tab-example">
+                            <Tab eventKey="today" title="Today">
+                                {tabContent}
+                            </Tab>
+                            <Tab eventKey="week" title="This Week">
+                                {tabContent}
+                            </Tab>
+                            <Tab eventKey="all" title="All">
+                                {tabContent}
+                            </Tab>
+                        </Tabs>
+                        {/* <Card className='Recent-Users'>
+                            <Card.Header>
+                                <Card.Title as='h5'>Recent Users</Card.Title>
+                            </Card.Header>
+                            <Card.Body className='px-0 py-2'>
+                                <Table responsive hover>
+                                    <tbody>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Isabella Christensen</h6>
+                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>11 MAY 12:56</h6>
+                                        </td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Mathilde Andersen</h6>
+                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-red f-10 m-r-15"/>11 MAY 10:35</h6>
+                                        </td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar3} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Karla Sorensen</h6>
+                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>9 MAY 17:38</h6>
+                                        </td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Ida Jorgensen</h6>
+                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted f-w-300"><i className="fa fa-circle text-c-red f-10 m-r-15"/>19 MAY 12:56</h6>
+                                        </td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Albert Andersen</h6>
+                                            <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>21 July 12:56</h6>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card> */}
+                    </Col>
+                    <Col md={4} xl={2}>
+                        <Card className='card-event'>
+                            <Card.Body>
+                                <div className="row align-items-center justify-content-center">
+                                    <div className="col">
+                                        <h5 className="m-0">Upcoming Event</h5>
+                                    </div>
+                                    <div className="col-auto">
+                                        <label className="label theme-bg2 text-white f-14 f-w-400 float-right">34%</label>
+                                    </div>
+                                </div>
+                                <h2 className="mt-2 f-w-300">45<sub className="text-muted f-14">Competitors</sub></h2>
+                                <h6 className="text-muted mt-3 mb-0">You can participate in event </h6>
+                                <i className="fa fa-angellist text-c-purple f-50"/>
+                            </Card.Body>
+                        </Card>
+                        
+                    </Col>
+                    
+                </Row>
+            </Aux>
+        );
+    }
+}
+
+export default StockDashboard;
