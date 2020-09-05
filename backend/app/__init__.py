06f6846fa3bd291e2a4be182f0de796a2e316db5
yarkhinephyo <yarkhinephyo@gmail.com>
2020-09-05 11:28:31 +0800

    updated endpoints abit

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_restful import Api
 
-
 from resources import Spot, SpotList 
 from resources import Home
 from resources import Register
@@ -16,12 +15,12 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 api.add_resource(Home, '/')
-api.add_resource(SpotList, '/spots')
-api.add_resource(Spot, '/spots/<int:spot_id>')
-api.add_resource(Register, '/register')
-api.add_resource(Token, '/token')
-api.add_resource(HomeAuth, '/home_auth')
-api.add_resource(YahooPrice, '/stock_prices')
+api.add_resource(SpotList, '/api/spots')
+api.add_resource(Spot, '/api/spots/<int:spot_id>')
+api.add_resource(Register, '/api/register')
+api.add_resource(Token, '/api/token')
+api.add_resource(HomeAuth, '/api/home_auth')
+api.add_resource(YahooPrice, '/api/stock_prices')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
