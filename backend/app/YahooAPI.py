362ed51fdae477229c9736f26dd4fd5169d307e7
Wa Thone <wathone98@gmail.com>
2020-09-06 03:20:19 +0800

    Adding of allSPFH
    Adding function for uploading the S&P500

--- backend/app/YahooAPI.py
+++ backend/app/YahooAPI.py
@@ -1,5 +1,6 @@
 import requests
 from Config import YAHOO_API_KEY
+import sys
 
 def get_response(url, querystring):
   headers = {
@@ -21,10 +22,10 @@ def get_yf_id(query):
   url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/auto-complete'
   querystring = {"lang":"en","region":"US","query": query}
 
-  response = get_response(url, querystring).json()
+  response = get_response(url, querystring)
 
   try:
-    return response['ResultSet']['Result'][0]['symbol']
+    return response.json()['ResultSet']['Result'][0]['symbol']
   except:
     return ''
 
@@ -36,22 +37,31 @@ def get_yf_analysis(stock_id):
   url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-analysis"
   querystring = {"symbol": stock_id}
 
-
-  response = get_response(url, querystring).json()
-
   return_dict = {
     "Revenue +1y": None,
     "Gross Profit": None
   }
+    
+  try:
+    response = get_response(url, querystring).json()
+  except:
+    print("Unexpected error:", sys.exc_info()[0], "for yf analysis", stock_id)
+    return return_dict
 
   if response == []:
     return return_dict
-  else:
+
+  try:
     for trend in response['earningsTrend']['trend']:
       if trend['period'] == '+1y':
         return_dict['Revenue +1y'] = trend['revenueEstimate']['avg']['raw']
+  except:
+    print("Unexpected error for Revenue +1y:", sys.exc_info()[0], "for yf analysis", stock_id)
 
+  try:
     return_dict['Gross Profit'] = response['financialData']['grossProfits']['raw']
+  except:
+    print("Unexpected error for Gross Profit", sys.exc_info()[0], "for yf analysis", stock_id)
 
   return return_dict
 
@@ -72,8 +82,11 @@ def get_yf_financials(stock_id):
 
   if response == []:
     return return_dict
-  else:
+    
+  try:
     return_dict['Beta'] = response['summaryDetail']['beta']['raw']
+  except:
+    print("Unexpected error for Beta", sys.exc_info()[0], "for yf financials", stock_id)
 
   return return_dict
 
