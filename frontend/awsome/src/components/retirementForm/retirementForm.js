6e36e0df4cf6f56132c68071bee1ce27928afc2f
tammi <tammichng.co@gmail.com>
2020-09-05 23:24:59 +0800

    update retirement form

--- frontend/awsome/src/components/retirementForm/retirementForm.js
+++ frontend/awsome/src/components/retirementForm/retirementForm.js
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom"
 import "./retirementForm.css"
 
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
-
+import Parser from "html-react-parser"
 
 // fake data generator
 const getItems = (count, offset = 0) =>
@@ -38,8 +38,17 @@ const move = (source, destination, droppableSource, droppableDestination) => {
     destClone.splice(droppableDestination.index, 0, removed);
 
     const result = {};
+
+    // if (droppableSource.droppableId == "droppable") {
+    //     result[droppableDestination.droppableId] = destClone;
+    // } else {
+    //     result[droppableSource.droppableId] = sourceClone;
+    //     result[droppableDestination.droppableId] = destClone;
+    // }
+
     result[droppableSource.droppableId] = sourceClone;
     result[droppableDestination.droppableId] = destClone;
+    
 
     return result;
 };
@@ -54,13 +63,14 @@ const getItemStyle = (isDragging, draggableStyle) => ({
 
     // change background colour if dragging
     background: isDragging ? '#FFA861' : 'white',
+    border: "#5CD4EF solid 2px",
 
     // styles we need to apply on draggables
     ...draggableStyle
 });
 
 const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? '#5CD4EF' : 'lightgrey',
+    background: isDraggingOver ? '#FFD6AB' : '#5CD4EF',
     padding: grid,
     width: 150,
     height: "422px"
@@ -68,23 +78,106 @@ const getListStyle = isDraggingOver => ({
 });
 
 const getOriginalListStyle = isDraggingOver => ({
-    background: isDraggingOver ? '#5CD4EF' : 'lightgrey',
+    background: isDraggingOver ? '#FFD6AB' : '#5CD4EF',
     padding: grid,
     width: 150,
+    overflow: "scroll",
     height: "400px",
 
 });
 
 export default class RetirementForm extends Component {
 
-    state = {
-        items: [{ id: "item-0", content: "Car" }, { id: "item-1", content: "Marraige" }, { id: "item-2", content: "Child" }],
-        selected1: [],
-        selected2: [],
-        selected3: [],
-        selected4: [],
-        selected5: []
-    };
+    bto = {"3" : "$150,000", "4":"$300,000", "5":"$400,000"}
+
+    constructor() {
+        super()
+
+
+        this.state = {
+            btoroom: "$150,000",
+            resaleroom: "$350,000",
+            condoroom: "$640,000",
+            privatecondoroom: "$800,000",
+            children: 0,
+            items: [
+                { id: "item-0", content: "Car $70,000" }, 
+                { id: "item-1", content: "University $40,000" }, 
+                { id: "item-2", content: <><div>Children<input className='drag-input' type='number' name='children' placeholder="no." onChange={this.handleInputChange} ></input><div id="children"></div></div></> }, 
+                { id: "item-3", content: "Wedding $30,000" },  
+                { id: "item-4", content: <><div>BTO Flat
+                                            <select className='drag-input' type='number' placeholder="no. rms" name='btoroom' onChange={this.handleInputChange} >
+                                                <option value="$150,000" active="true">3 rms</option>
+                                                <option value="$300,000">4 rms</option>
+                                                <option value="$400,000">5 rms</option>
+                                            </select>
+                                            <div id="bto">$150,000</div></div></> }, 
+                { id: "item-5", content: <><div>Resale Flat
+                                            <select className='drag-input' type='number' placeholder="no. rms" name='resaleroom' onChange={this.handleInputChange} >
+                                                <option value="$350,000" active="true">3 rms</option>
+                                                <option value="$420,000">4 rms</option>
+                                                <option value="$520,000">5 rms</option>
+                                            </select>
+                                            <div id="resale">$350,000</div></div>
+                                            </> },  
+                { id: "item-6", content: <><div>Executive Condo
+                                            <select className='drag-input' type='number' placeholder="no. rms" name='condoroom' onChange={this.handleInputChange} >
+                                                <option value="$640,000" active="true">2 rms</option>
+                                                <option value="$780,000">3 rms</option>
+                                                <option value="$1,100,000">4 rms</option>
+                                                <option value="$1,600,000">5 rms</option>
+                                                <option value="$1,900,000">Penthouse</option>
+                                            </select>
+                                            <div id="ex-condo">$640,000</div></div></> }, 
+                { id: "item-7", content: <><div>Private Condo
+                                            <select className='drag-input' type='number' placeholder="no. rms" name='privatecondoroom' onChange={this.handleInputChange} >
+                                                <option value="$800,000" active="true">2 rms</option>
+                                                <option value="$970,000">3 rms</option>
+                                                <option value="$1,400,000">4 rms</option>
+                                                <option value="$2,000,000">5 rms</option>
+                                                <option value="$3,000,000">Penthouse</option>
+                                            </select>
+                                            <div id="priv-condo">$800,000</div></div></> },
+                { id: "item-8", content: "Renovations $60,000"}],
+            selected1: [],
+            selected2: [],
+            selected3: [],
+            selected4: [],
+            selected5: [],
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        var value = target.value;
+        const name = target.name; 
+        
+        if (name == "btoroom") {
+            document.querySelector("#bto").innerHTML = value;
+        }
+
+        if (name == "resaleroom") {
+            document.querySelector("#resale").innerHTML = value;
+        }
+
+        if (name == "condoroom") {
+            document.querySelector("#ex-condo").innerHTML = value;
+        }
+
+        if (name == "privatecondoroom") {
+            document.querySelector("#priv-condo").innerHTML = value;
+        }
+
+        if (name == "children") {
+            document.querySelector("#children").innerHTML = "$" + (value * 400000).toLocaleString();
+        }
+
+        this.setState({
+            [name] : value
+        })
+    }
 
 
     /**
@@ -160,7 +253,6 @@ export default class RetirementForm extends Component {
                         selected1 : result[droppable]
                     })
                 } else if (droppable === "droppable3") {
-                    console.log(result[droppable])
                     this.setState({
                         selected2 : result[droppable]
                     })
@@ -182,11 +274,40 @@ export default class RetirementForm extends Component {
         }
     };
 
+    handleCalculation = () => {
+        var total = 0
+        var all = this.state.selected1.concat(this.state.selected2, this.state.selected3, this.state.selected4);
+        all.forEach(item => {
+            console.log(item, typeof item["content"])
+            if (typeof item["content"] == "string") {
+                total += parseInt(item["content"].replace(',', "").split(" ")[1].substr(1))
+            } else {
+                if (this.state.children > 0) {
+                    total += this.state.children * 400000
+                }
+    
+                Object.keys(this.state).forEach((i) => {
+                    if (i.includes("room") && i == item["content"].props["children"].props["children"][1].props.name) {
+                        total += parseInt(this.state[i].replace(',', "").substr(1))
+                    }
+                })
+            }
+
+            
+
+        });
+        console.log("total", total)
+        console.log(document.querySelector("#totalneeded").innerHTML)
+        document.querySelector("#totalneeded").innerHTML = "$" + (total).toLocaleString()
+
+    }
+
 
 
 
     render() {
 
+        console.log(this.state.btoroom)
         return (
             <Container fluid>
 
@@ -213,8 +334,12 @@ export default class RetirementForm extends Component {
                                             <Form.Control type="number" placeholder="62"></Form.Control>
                                         </Form.Group>
                                     </Col>
+                                    <Col xs={3}>
+                                        <h3>Total Needed:</h3>
+                                        <h3 id="totalneeded">$0</h3>
+                                    </Col>
                                     <Col>
-
+                                        <Button id="submit" variant="primary" onClick={this.handleCalculation}>Calculate</Button>
                                     </Col>
                                 </Row>
 
@@ -224,7 +349,7 @@ export default class RetirementForm extends Component {
                                 <Row>
                                     <DragDropContext onDragEnd={this.onDragEnd}>
                                         <Col className="center">
-                                            <h4>Drag and drop to plan</h4>
+                                            <h4>Drag and drop<br></br>to plan</h4>
                                             <Droppable id="droppable" droppableId="droppable">
                                                 {(provided, snapshot) => (
                                                     <div
@@ -348,7 +473,7 @@ export default class RetirementForm extends Component {
                                             </Droppable>
                                         </Col>
                                         <Col className="center">
-                                            <h4>> 60 yrs old</h4>
+                                            <h4> 60 yrs old</h4>
                                             <Droppable droppableId="droppable5">
                                                 {(provided, snapshot) => (
                                                     <div
@@ -384,9 +509,11 @@ export default class RetirementForm extends Component {
 
                                 <br></br><br></br>
 
+                                
                                 <Link to="/equityResult">
                                     <Button id="submit" variant="primary" type="submit">Next</Button>
                                 </Link>
+                                
                             </Form>
                         </div>
 
